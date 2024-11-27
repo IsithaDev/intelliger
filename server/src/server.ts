872c56cdc424887f00df77c3dda63fb47ad54091
IsithaDev <isitha.dev@gmail.com>
2024-11-28 01:21:24 +0530

    feat(mongodb): connected to the mongodb
    Connected to the mongodb database using database url
    
    Closes #2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { createServer } from "http";
 
 import app from "./app";
@@ -6,6 +7,12 @@ import app from "./app";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const mongodb_url = process.env.MONGODB_URL;
+
+mongoose
+  .connect(mongodb_url, { dbName: "intelliger" })
+  .then(() => console.log("Mongodb connected!"))
+  .catch((error) => console.log(error));
 
 const server = createServer(app);
 
