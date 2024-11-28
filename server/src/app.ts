6c1164464ebfc31860ca240d3ee7ffb8b831370a
IsithaDev <isitha.dev@gmail.com>
2024-11-28 07:54:15 +0530

    feat(error): create global error handler
    Create custom appError class.
    Create basic globalErrorHandler for manage errors.
    Create catchAsync function for catch errors.
    
    Closes #4

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,19 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import globalErrorHandler from "./controllers/errorController";
+import AppError from "./utils/appError";
 
 const app = express();
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  return next(
+    new AppError(`Cannot find ${req.originalUrl} on this server.`, 404)
+  );
+});
+
+app.use(globalErrorHandler);
 
 export default app;
---
+++ server/src/controllers/errorController.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+export default globalErrorHandler;
